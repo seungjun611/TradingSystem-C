966c05d9782f6a21517c1e30da4c43c1ab36d3a7
ljbsps <ljbsps@naver.com>
2024-05-21 14:12:22 +0900

    add parameter name

--- TradingSystem/StockBroker.cpp
+++ TradingSystem/StockBroker.cpp
@@ -5,6 +5,6 @@ class StockBroker
 public:
 	virtual void login(std::string id, std::string pw) = 0;
 	virtual void buy(std::string stockCode, int price, int count) = 0;
-	virtual void sell(std::string stockCode, int, int count) = 0;
+	virtual void sell(std::string stockCode, int price, int count) = 0;
 	virtual int getPrice(std::string stockCode) = 0;
 };
