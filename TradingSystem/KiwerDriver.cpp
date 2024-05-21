dc71ca4069a2d15c85544d977d7bc7e06c3c08be
beomgyu.heo <0105086@naver.com>
2024-05-21 14:21:34 +0900

    KiwerDriver_branch

--- TradingSystem/KiwerDriver.cpp
+++ TradingSystem/KiwerDriver.cpp
@@ -1,8 +1,32 @@
 #include "KiwerAPI.cpp"
 #include "StockBroker.cpp"
+#include <string>
+
 
 class KiwerDriver : public StockBroker
 {
-public:
+private:
+	KiwerAPI* kiwerAPI;
 
+public:
+	KiwerDriver(KiwerAPI* kiwerAPI)
+	{
+		this->kiwerAPI = kiwerAPI;
+	}
+	void login(std::string id, std::string pw) override
+	{
+		kiwerAPI->login(id, pw);
+	}
+	void buy(std::string stockCode, int price, int count) override
+	{
+		kiwerAPI->buy(stockCode, price, count);
+	}
+	void sell(std::string stockCode, int price, int count) override
+	{
+		kiwerAPI->sell(stockCode, price, count);
+	}
+	int getPrice(std::string stockCode) override
+	{
+		kiwerAPI->currentPrice(stockCode);
+	}
 };
