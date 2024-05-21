f077735f682ba2993538f0dabcb22ef5b69f883b
jinwk.lee <jinwk.lee@samsung.com>
2024-05-21 15:33:03 +0900

    add buyNiceTiming

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,6 +3,68 @@
 #include "gtest/gtest.h"
 #include "../TradingSystem/TradingSystem.cpp"
 
-TEST(TradingSystem, SelectStockBroker) {
-	TradingSystem system;
+using namespace testing;
+
+class MockTrading : public TradingSystem {
+public:
+	MOCK_METHOD(int, getPrice, (int item), (override));
+	MOCK_METHOD(bool, buy, (int item,int price, int count), (override));
+};
+
+
+TEST(TradingSystem, buyNiceTimingGetPrice3times) {
+	MockTrading system;
+
+	EXPECT_CALL(system, getPrice(1))
+		.Times(3)
+		.WillOnce(Return(10))
+		.WillOnce(Return(20))
+		.WillOnce(Return(30))
+		;
+
+	system.buyNiceTiming(1, 30);
+}
+
+
+TEST(TradingSystem, buyNiceTimingSuccess) {
+	MockTrading system;
+
+	EXPECT_CALL(system, getPrice(1))
+		.Times(3)
+		.WillOnce(Return(10))
+		.WillOnce(Return(20))
+		.WillOnce(Return(30))
+		;
+
+	EXPECT_CALL(system, buy(1, 30, 1))
+		.WillOnce(Return(true));
+
+	system.buyNiceTiming(1, 30);
+}
+
+TEST(TradingSystem, buyNiceTimingFail) {
+	MockTrading system;
+
+	EXPECT_CALL(system, getPrice(1))
+		.Times(3)
+		.WillOnce(Return(10))
+		.WillOnce(Return(20))
+		.WillOnce(Return(20))
+		;
+
+	system.buyNiceTiming(1, 30);
+}
+
+
+TEST(TradingSystem, buyNiceTimingException) {
+	MockTrading system;
+
+	EXPECT_CALL(system, getPrice(1))
+		.Times(3)
+		.WillOnce(Return(10))
+		.WillOnce(Return(20))
+		.WillOnce(Return(0))
+		;
+	
+	EXPECT_THROW(system.buyNiceTiming(1, 30), exception);
 }
--- TradingSystem/TradingSystem.cpp
+++ TradingSystem/TradingSystem.cpp
@@ -1,5 +1,6 @@
 #include "StockBroker.cpp"
 #include <string>
+#include <exception>
 using namespace std;
 
 class TradingSystem
@@ -7,10 +8,53 @@ class TradingSystem
 public:
 	void selectStockBrocker(string api);
 	bool login(int id, int password);
-	bool buy(int item, int price, int count);
+	virtual bool buy(int item, int price, int count) = 0;
 	bool sell(int item, int price, int count);
-	int getPrice(int item);
+	virtual int getPrice(int item) = 0;
+	void buyNiceTiming(int item, int price);
+	void sellNiceTiming(int item, int count);
+
+	int checkPrice3Times(int price_list[3],int item);
+	int checkPriceUp(int price_list[3]);
 
 private:
 	StockBroker* _broker{ nullptr };
 };
+
+int TradingSystem::checkPrice3Times(int price_list[3],int item) {
+	int priceNow = 0;
+	for (int i = 0; i < 3; i++) {
+		price_list[i] = getPrice(item);
+		priceNow = price_list[i];
+	}
+
+	return priceNow;
+}
+
+int TradingSystem::checkPriceUp(int price_list[3]) {
+	int priceUpCount = 0;
+	for (int i = 1; i < 3; i++) {
+		if (price_list[i - 1] == 0 || price_list[i] == 0) {
+			throw exception("Item Price is 0!!");
+		}
+
+		if (price_list[i - 1] < price_list[i]) {
+			priceUpCount++;
+		}
+	}
+
+	return priceUpCount;
+}
+
+void TradingSystem::buyNiceTiming(int item, int money)
+{
+	int price_list[3];
+	int priceNow = 0;
+	priceNow = checkPrice3Times(price_list, item);
+
+	int priceUpCount = checkPriceUp(price_list);
+
+	if (priceUpCount == 2) {
+		buy(item, priceNow, money / priceNow);
+	}
+}
